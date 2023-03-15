3aedf12fae3fd75a83d9fecd4cadc79537c56323
zfor <forisz.zisis@gmail.com>
2023-03-15 22:45:40 +0100

    Maybe fix 2

---
+++ .changeset/funny-panthers-behave.md
@@ -0,0 +1,5 @@
+---
+"@czisis/pgconv": patch
+---
+
+Initial commit
--- src/partners/partners.ts
+++ src/partners/partners.ts
@@ -1,1 +1,8 @@
-export const extractPartners = () => console.log("extracting partners");
+import { readFileSync } from "fs";
+import { XMLParser } from "fast-xml-parser";
+
+export const extractPartners = (inputFilePath: string) => {
+  const xmlSource = readFileSync(inputFilePath, { encoding: "utf-8" });
+  //const parser = new XMLParser();
+  console.log(xmlSource);
+};
