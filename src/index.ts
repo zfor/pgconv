610968e318572dfc0cc5db8253b9c9b861ed43d8
zfor <forisz.zisis@gmail.com>
2023-03-15 22:45:40 +0100

    Maybe fix 2

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { program } from "commander";
 import { extractPartners } from "./partners/partners";
 
-program.command("partners").action(() => extractPartners());
+program
+  .command("partners")
+  .argument("<input-file>")
+  .action((...args: any[]) => extractPartners(args[0]));
 
 program.parse();
